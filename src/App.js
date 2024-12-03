1579c7870e731f1594a88d2e175f9a4a16087404
sreenadh34 <43.sreenadh@gmail.com>
2024-12-04 00:59:42 +0530

    add Pizza component and update App to render multiple pizzas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-const App =() => {
-    return React.createElement(
-        "div",
-        {},
-        React.createElement("h1",{},"Padre Gino's")
-    );
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Pixel Perfect Pizzas"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Mozzarella Cheese, Pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian Pizza",
+      description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+    }),
+    React.createElement(Pizza, {
+      name: "The Big Meat Pizza",
+      description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
