f95838252f2bd73635c0604c6bff9704bb48e5d8
sreenadh34 <43.sreenadh@gmail.com>
2024-12-06 02:35:00 +0530

    add Order component for pizza ordering functionality and update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's Pizza – Order Now</h1>
-      <Pizza name="Pepperoni" description="Mozzarella Cheese, Pepperoni" image={"/public/pizzas/pepperoni.webp"} />
-      <Pizza
-        name="The Hawaiian Pizza"
-        description="Sliced Ham, Pineapple, Mozzarella Cheese" image={"/public/pizzas/hawaiian.webp"}
-      />
-      <Pizza
-        name="The Big Meat Pizza"
-        description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage" image={"/public/pizzas/big_meat.webp"}
-      />
+      <Order />
     </div>
   );
 };
