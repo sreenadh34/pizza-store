6507ad04c635661101240fb765f9780e93c0223a
sreenadh34 <43.sreenadh@gmail.com>
2024-12-09 01:16:19 +0530

    add PizzaOfTheDay

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
 
 
 const App = () => {
   return (
+    <StrictMode>
     <div>
       <h1>Padre Gino's Pizza – Order Now</h1>
       <Order />
     </div>
+    </StrictMode>
   );
 };
 
