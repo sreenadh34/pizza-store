b9848844068312c4118bd437709cb598dec76b23
sreenadh34 <43.sreenadh@gmail.com>
2024-12-10 01:04:47 +0530

    add logo class to header and implement useDebugValue in usePizzaOfTheDay hook

--- src/usePizzaOfTheDay.jsx
+++ src/usePizzaOfTheDay.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useDebugValue } from "react";
 
 export const usePizzaOfTheDay = () => {
   const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
-
+  useDebugValue(pizzaOfTheDay ? `${pizzaOfTheDay.name}` : "Loading...");
   useEffect(() => {
     async function fetchPizzaOfTheDay() {
       const response = await fetch("/api/pizza-of-the-day");
