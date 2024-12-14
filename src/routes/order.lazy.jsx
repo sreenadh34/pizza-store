e7c5bdecf618021e8af3f3854e0c39591117aaf1
sreenadh34 <43.sreenadh@gmail.com>
2024-12-15 02:05:13 +0530

    update dependencies to React 19 and refactor form handling in contact and order routes

--- src/routes/order.lazy.jsx
+++ src/routes/order.lazy.jsx
@@ -56,15 +56,16 @@ const intl = new Intl.NumberFormat("en-US", {
     setPizzaTypes(pizzasJson);
     setLoading(false);
   }
+  function addToCart() {
+    setCart([...cart, { pizza: selectedPizza, size: pizzaSize, price }]);
+  }
+
   return (
     <div className="order-page">
     <div className="order">
       <h2>Create Order</h2>
       <form
-  onSubmit={(e) => {
-    e.preventDefault();
-    setCart([...cart, { pizza: selectedPizza, size: pizzaSize, price }]);
-  }}
+      action={addToCart}
 >
         <div>
           <div>
